951702a5d804ea1f8b0b5ecb3c7addc765813b37
Rushan <rushan_oline@mail.ru>
2016-01-21 08:36:23 +0300

    add cell spec

---
+++ spec/models/cell.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe Cell do
+	let (:cell) { Cell.new(5, 3, false) }
+	let(:alive_cell) { Cell.new(2, 6, true) }
+	let(:dead_cell) { Cell.new(12, 4, false) }
+
+	describe "#initialize" do
+		subject { cell }
+
+		it "should be an instance of Cell" do
+			expect(subject).to be_a Cell
+		end
+	end
+
+	context "correct state" do
+		describe "#alive?" do
+			subject { alive_cell }
+
+			it "should be alive" do
+				expect(subject.alive?).to eq(true)
+			end
+		end
+
+		describe "#dead?" do
+			subject { dead_cell }
+
+			it "should be dead" do
+				expect(subject.dead?).to eq(true)
+			end
+		end
+	end
+
+	context "coordinates access" do
+		describe "#x" do
+			subject { cell }
+
+			it "should return a coordinate (x)" do
+				expect(subject.x).to eq(cell.x)
+			end
+
+			it "should return a coordinate (y)" do
+				expect(subject.y).to eq(cell.y)
+			end
+			
+		end
+	end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+$LOAD_PATH.unshift File.expand_path('../../.', __FILE__)
+
+require 'models/cell'
+RSpec.configure do |config|
+
+end
